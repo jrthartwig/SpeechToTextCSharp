2423dfcddbe285128f3dbbf4e3a8c05468a0549f
Jocelynn Hartwig <jhartwig@apterainc.com>
2021-01-30 12:56:37 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+using System;
+using System.Threading.Tasks;
+
+namespace SpeechToTextCsharp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await SpeechAI.RecognizeSpeechAsync();
+        }
+    }
+}
---
+++ SpeechAI.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
+namespace SpeechToTextCsharp
+{
+    public class SpeechAI
+    {
+        public static async Task RecognizeSpeechAsync()
+        {
+            var config = SpeechConfig.FromSubscription("04d78025d6c14834ba9888b8d307843c", "eastus");
+
+            using (var audioInput = AudioConfig.FromWavFileInput("./hawking01.wav"))
+
+            using (var recognizer = new SpeechRecognizer(config, audioInput))
+            {
+                Console.WriteLine("Recognizing first result...");
+                var result = await recognizer.RecognizeOnceAsync();
+
+                switch (result.Reason)
+                {
+                    case ResultReason.RecognizedSpeech:
+                        Console.WriteLine($"We recognized: {result.Text}");
+                        break;
+
+                    case ResultReason.NoMatch:
+                        Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                        break;
+
+                    case ResultReason.Canceled:
+                        var cancellation = CancellationDetails.FromResult(result);
+                        Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                        if (cancellation.Reason == CancellationReason.Error)
+                        {
+                            Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                            Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                            Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
